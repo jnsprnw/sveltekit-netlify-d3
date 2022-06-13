f0d622e177aae78370a3d3f05de33705ed4fa6fe
jnsprnw <6gqrbj@posteo.org>
2022-06-14 00:27:34 +0200

    Clean indention

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,17 @@
 <script>
-	// import { hsl } from 'd3-color';
-	// import { interpolateNumber } from 'd3-interpolate';
-	// import { scaleLinear } from 'd3-scale';
-	import { scaleLinear } from 'd3';
+  // import { hsl } from 'd3-color';
+  // import { interpolateNumber } from 'd3-interpolate';
+  // import { scaleLinear } from 'd3-scale';
+  import { scaleLinear } from 'd3';
 
-	// const c = hsl('steelblue');
-	// c.h += 90;
-	// c.s += 0.2;
-	// const color = c + '';
+  // const c = hsl('steelblue');
+  // c.h += 90;
+  // c.s += 0.2;
+  // const color = c + '';
 
-	// const i = interpolateNumber(10, 20);
+  // const i = interpolateNumber(10, 20);
 
-	const x = scaleLinear()
+  const x = scaleLinear()
     .domain([10, 130])
     .range([0, 960]);
 </script>
