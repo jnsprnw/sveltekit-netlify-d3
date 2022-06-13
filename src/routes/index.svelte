cac0a69357a2fd0c13e136cc946cecdf00efe6ff
jnsprnw <6gqrbj@posteo.org>
2022-06-13 22:25:50 +0200

    Init

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { hsl } from 'd3-color';
+	import { interpolateNumber } from 'd3-interpolate';
+	import { scaleLinear } from 'd3-scale';
+
+	const c = hsl('steelblue');
+	c.h += 90;
+	c.s += 0.2;
+	const color = c + '';
+
+	const i = interpolateNumber(10, 20);
+
+	const x = scaleLinear()
+    .domain([10, 130])
+    .range([0, 960]);
+</script>
+
+<h1>{ color }</h1>
+<h1>{ i(0.2) }</h1>
+<h1>{ x(50) }</h1>
