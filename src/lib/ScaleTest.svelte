db607dc757f853fe0868f6b4d267fcec13cb5e31
jnsprnw <6gqrbj@posteo.org>
2022-06-14 10:21:47 +0200

    Component test

---
+++ src/lib/ScaleTest.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { scaleLinear } from 'd3-scale';
+
+	const x = scaleLinear()
+    .domain([10, 130])
+    .range([0, 960]);
+</script>
+
+<h1>{ x(50) }</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,10 @@
 <script>
+  import ScaleTest from '$lib/ScaleTest.svelte';
   // import { hsl } from 'd3-color';
   // import { interpolateNumber } from 'd3-interpolate';
   // import { scaleLinear } from 'd3-scale';
   // import { scaleLinear } from 'd3';
-  import * as d3 from 'd3';
+  // import * as d3 from 'd3';
 
   // const c = hsl('steelblue');
   // c.h += 90;
@@ -12,11 +13,12 @@
 
   // const i = interpolateNumber(10, 20);
 
-  const x = d3.scaleLinear()
-    .domain([10, 130])
-    .range([0, 960]);
+  // const x = d3.scaleLinear()
+  //   .domain([10, 130])
+  //   .range([0, 960]);
 </script>
 
 <!-- <h1>{ color }</h1> -->
 <!-- <h1>{ i(0.2) }</h1> -->
-<h1>{ x(50) }</h1>
+<!-- <h1>{ x(50) }</h1> -->
+<ScaleTest />
